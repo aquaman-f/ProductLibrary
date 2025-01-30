c6638c0d6806acc5940bdca0709c4f24c8676dad
aquaman-f <81656346+aquaman-f@users.noreply.github.com>
2025-01-30 18:29:38 +0200

    Add files via upload

---
+++ ProductTests.cs
@@ -0,0 +1,39 @@
+﻿using ProductLibrary;
+using System;
+using Xunit;
+
+namespace ProductLibrary.Tests
+{
+	public class ProductTests
+	{
+		[Fact]
+		public void ApplyDiscount_Returns_CorrectPrice_ForValidDiscount()
+		{
+			//Arrange
+			var product = new Product(100);
+			double discountPercentage = 20;
+
+			//Act
+			var result = product.ApplyDiscount(discountPercentage);
+
+			//Assert
+			Assert.Equal(80, result);
+		}
+
+		[Theory]
+		[InlineData(100, 0, 100)]
+        [InlineData(100, 100, 0)]
+        [InlineData(100, -10, 100)] //Ei muuta hintaa virheellisellä syötteellä
+		public void ApplyDiscount_HandlesEdgeCases(double price, double discountPercentage, double expected)
+		{
+			//Arrange
+			var product = new Product(price);
+
+			//Act
+			var result = product.ApplyDiscount(discountPercentage);
+
+			//Assert
+			Assert.Equal(expected, result);
+		}
+    }
+}
