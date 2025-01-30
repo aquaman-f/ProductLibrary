120c71a8833a17694009ab64aa6df3778a443d49
aquaman-f <jt.tuorila@gmail.com>
2025-01-30 18:28:37 +0200

    Add project files.

---
+++ Product.cs
@@ -0,0 +1,22 @@
+﻿namespace ProductLibrary
+{
+    public class Product
+    {
+        public double Price { get; set; }
+
+        public Product(double price)
+        {
+            Price = price;
+        }
+
+        public double ApplyDiscount(double discountPercentage)
+        {
+            //Placeholder
+            if (discountPercentage < 0)
+            {
+                discountPercentage = 0;
+            }
+            return Price - (Price * ( discountPercentage / 100));
+        }
+    }
+}
